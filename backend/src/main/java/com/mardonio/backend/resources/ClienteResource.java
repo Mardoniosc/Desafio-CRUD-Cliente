f7db161e28957359ccabaa411380ce0fea7afcda
Mardonio Costa <mardonio.costa@spread.com.br>
2021-04-09 15:56:48 -0300

    Listando todos os clientes paginados

--- backend/src/main/java/com/mardonio/backend/resources/ClienteResource.java
+++ backend/src/main/java/com/mardonio/backend/resources/ClienteResource.java
@@ -4,12 +4,14 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -46,6 +48,17 @@ public class ClienteResource {
 	@GetMapping
 	public ResponseEntity<List<Cliente>> findAll() {
 		List<Cliente> list = clienteService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping("/page")
+	public ResponseEntity<Page<Cliente>> findPage(
+				@RequestParam(value = "page", defaultValue = "0" ) Integer page,
+				@RequestParam(value = "linesPerPage", defaultValue = "24" ) Integer linesPerPage,
+				@RequestParam(value = "orderBy", defaultValue = "nome" ) String orderBy,
+				@RequestParam(value = "direction", defaultValue = "ASC" ) String direction
+			) {
+		Page<Cliente> list = clienteService.findPage(page, linesPerPage, orderBy, direction);
 		return ResponseEntity.ok().body(list);
 	}
 
--- backend/src/main/java/com/mardonio/backend/services/ClienteService.java
+++ backend/src/main/java/com/mardonio/backend/services/ClienteService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.mardonio.backend.domain.Cliente;
@@ -33,6 +36,11 @@ public class ClienteService {
 	
 	public List<Cliente> findAll() {
 		return clienteRepository.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return clienteRepository.findAll(pageRequest);
 	}
 	
 }
