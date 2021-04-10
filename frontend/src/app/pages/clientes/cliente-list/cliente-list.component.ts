0cc0fbaf17d2fef0d78c6956fa0800da660b9d78
Mardonio Costa <mardonio.costa@spread.com.br>
2021-04-09 23:00:47 -0300

    Concluindo a implementaçao do ClienteList

--- frontend/src/app/pages/clientes/cliente-list/cliente-list.component.ts
+++ frontend/src/app/pages/clientes/cliente-list/cliente-list.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../shared/models/cliente.model';
+import { ClienteService } from '../shared/services/cliente.service';
 
 @Component({
   selector: 'app-cliente-list',
   templateUrl: './cliente-list.component.html',
-  styleUrls: ['./cliente-list.component.css']
+  styleUrls: ['./cliente-list.component.css'],
 })
 export class ClienteListComponent implements OnInit {
+  clientes: Cliente[] = [];
 
-  constructor() { }
+  constructor(private clienteService: ClienteService) {}
 
   ngOnInit(): void {
+    this.clienteService.getAll().subscribe(
+      (data) => (this.clientes = data),
+      (err) => console.error('Erro ao carregar clientes', err)
+    );
   }
 
+  deleteCliente(cliente: Cliente) {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
+
+    if (mustDelete) {
+      this.clienteService.delete(cliente.id).subscribe(
+        (data) => {
+          this.clientes = this.clientes.filter((x) => x != cliente);
+        },
+        (err) => alert('Erro ao tentar excluir!!')
+      );
+    }
+  }
 }
--- frontend/src/app/pages/clientes/clientes.module.ts
+++ frontend/src/app/pages/clientes/clientes.module.ts
@@ -4,9 +4,6 @@ import { CommonModule } from '@angular/common';
 import { ClientesRoutingModule } from './clientes-routing.module';
 import { ClienteListComponent } from './cliente-list/cliente-list.component';
 import { ClienteFormComponent } from './cliente-form/cliente-form.component';
-import { HttpClientModule } from '@angular/common/http';
-
-
 @NgModule({
   declarations: [
     ClienteListComponent,
@@ -15,7 +12,6 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     CommonModule,
     ClientesRoutingModule,
-    HttpClientModule
   ]
 })
 export class ClientesModule { }
