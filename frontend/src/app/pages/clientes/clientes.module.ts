d71c0b0676d4484f49caabe60752ef9e635f0fca
Mardonio Costa <mardonio.costa@spread.com.br>
2021-04-09 22:44:59 -0300

    Ativando o interceptador HTTP do angularInMemoryWebAPI

--- frontend/src/app/pages/clientes/clientes.module.ts
+++ frontend/src/app/pages/clientes/clientes.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ClientesRoutingModule } from './clientes-routing.module';
 import { ClienteListComponent } from './cliente-list/cliente-list.component';
 import { ClienteFormComponent } from './cliente-form/cliente-form.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { ClienteFormComponent } from './cliente-form/cliente-form.component';
   ],
   imports: [
     CommonModule,
-    ClientesRoutingModule
+    ClientesRoutingModule,
+    HttpClientModule
   ]
 })
 export class ClientesModule { }
