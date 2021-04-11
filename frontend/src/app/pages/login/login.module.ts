cfbfd2700337b6f7cbeac84c8216a07fc5701036
Mardonio Costa <mardonio.costa@spread.com.br>
2021-04-11 16:07:40 -0300

    Criando a funcionalidade de login no componente

--- frontend/src/app/pages/login/login.module.ts
+++ frontend/src/app/pages/login/login.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { UserComponent } from './user/user.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { UserComponent } from './user/user.component';
   ],
   imports: [
     CommonModule,
+    SharedModule,
     LoginRoutingModule
   ]
 })
--- frontend/src/app/pages/login/user/user.component.ts
+++ frontend/src/app/pages/login/user/user.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import toastr from 'toastr';
+import { Login } from '../shared/login.model';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
+    this.gerarForm();
   }
 
+  gerarForm() {
+    this.form = this.fb.group({
+      email: [null, Validators.required],
+      senha: [null, Validators.required]
+    })
+  }
+
+  logar() {
+    if(this.form.invalid) {
+      toastr.error('Dados inválidos!')
+      return;
+    }
+    const login: Login = this.form.value;
+  }
 }
