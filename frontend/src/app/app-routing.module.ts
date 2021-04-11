221cf9c8b05a1a15844edc828a04971653dcab4b
Mardonio Costa <mardonio.costa@spread.com.br>
2021-04-11 14:46:55 -0300

    Adicionando tela home

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./pages/clientes/clientes.module').then((m) => m.ClientesModule),
   },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
 ];
 
 @NgModule({
